6df53df065aa3ecc2c068d5def9c29c3304e88f3
Ippei Oshiro <i_oshiro@zenk.co.jp>
2021-04-17 18:49:17 +0900

    HOMEにボタン追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>ホーム</h1>
+    <p>ホーム画面</p>
+    <p><roter-link to="sign-in">サインイン</roter-link></p>
+    <p><button @click="calendar">カレンダー</button></p>
+    <p><button @click="profile">プロフィール</button></p>
+    <p><button @click="share">共有</button></p>
   </div>
 </template>
 
